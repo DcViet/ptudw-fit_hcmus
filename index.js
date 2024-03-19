6ec246605768b2d4fe2163a0241c075529bccac1
DcViet <tranducviet.eng@gmail.com>
2024-03-20 00:07:02 +0700

    feat: hbs

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const expressHandlebars = require('express-handlebars');
+
+//cau hinh public static folder
+app.use(express.static(__dirname + '/public'))
+
+//cau hinh su dung express-handlebars
+app.engine('hbs', expressHandlebars.engine ({
+    layoutsDir: __dirname + 'views/layouts',
+    partialsDir: __dirname + 'views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine','hbs');
+
+//routes
+app.get('/',(req, res) => {
+    res.render(index);
+})
+// app.set('view', __dirname + '/view');
+// khoi dong web server
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
