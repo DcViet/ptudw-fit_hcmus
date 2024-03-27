85e6a97015e5ae4ce0434786e928476864d8be9d
DcViet <tranducviet.eng@gmail.com>
2024-03-27 20:12:25 +0700

    feat: add products to cart

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const expressHandlebars = require('express-handlebars');
 const { routes } = require('./routes/indexRouter');
 const {createStarList} = require('./controllers/handlebarsHelper');
 const {createPagination} = require('express-handlebars-paginate');
+const session = require('express-session');
 
 //cau hinh public static folder
 app.use(express.static(__dirname + '/public'));
@@ -28,6 +29,30 @@ app.engine('hbs', expressHandlebars.engine({
 app.set('view engine', 'hbs');
 // Error: ENOENT: no such file or directory, open '/Users/cps/Downloads/Git_repo/ptudw-21880291views/layouts/layout.hbs' 
 // lỗi do thiếu /views
+
+//cau hinh doc du lieu post tu body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cau hinh su dung session
+app.use(session({
+    secret: 'S3cret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 20 * 60 * 1000 //20ph
+    }
+}));
+
+//middleware khoi dong gio hang
+app.use((req, res, next) => {
+    let Cart = require('./controllers/cart');
+    req.session.cart = new Cart(req.session.cart ? req.session.cart : {});
+    res.locals.quantity = req.session.cart.quantity;
+    
+    next();
+});
 
 //routes
 app.use('/', require('./routes/indexRouter'));
