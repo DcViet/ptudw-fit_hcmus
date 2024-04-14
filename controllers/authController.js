0eda9e5f6219951f4e4f453603c60041952c12ef
DcViet <tranducviet.eng@gmail.com>
2024-04-14 15:46:21 +0700

    feat:bao mat truy xuat

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,32 +5,48 @@ const passport = require('./passport');
 
 controller.show = (req, res) => {
 
-    console.log("Giá trị của loginMessage:", req.flash('loginMessage'));
-    res.render('login', { loginMessage: req.flash('loginMessage') });
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    res.render('login', { loginMessage: req.flash('loginMessage'), reqUrl: req.query.reqUrl });
 }
 
 controller.login = (req, res, next) => {
     let keepSignedIn = req.body.keepSignedIn;
+    let reqUrl = req.body.reqUrl ? req.body.reqUrl : '/users/my-account';
+    let cart = req.session.cart; 
     passport.authenticate('local-login', (error, user) => {
         if (error) {
             return next(error);
         }
         if (!user) {
-            return res.redirect('/users/login');
+            return res.redirect(`/users/login?reqUrl=${reqUrl}`);
         }
         req.logIn(user, (error) => {
             if (error) { return next(error); }
             req.session.cookie.maxAge = keepSignedIn ? (24 * 60 * 60 * 1000) : null;
-            return res.redirect('/users/my-account');
+            req.session.cart = cart;
+            return res.redirect(reqUrl);
         });
     })(req, res, next);
 
 }
 
 controller.logout = (req, res, next) => {
+    let cart = req.session.cart;
     req.logout((error) => {
         if (error) { return next(error); }
+        req.session.cart = cart;
         res.redirect('/');
     });
 }
+
+controller.isLoggedIn = (req, res, next) => {
+
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect(`/users/login?reqUrl=${req.originalUrl}`);
+}
+
 module.exports = controller;
