58d6586d52833be31abcc8ea7d2f0d3ba301859f
DcViet <tranducviet.eng@gmail.com>
2024-04-15 02:50:36 +0700

    feat: forgot password

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -64,25 +64,25 @@ router.post('/forgot',
 );
 
 
-router.get('/reset', controller.showResetPassword);
-router.post('./reset',
+router.post('/reset',
     body('email').trim().notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid email address!'),
     body('password').trim().notEmpty().withMessage('Password is required!'),
     body('password').matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/).withMessage('Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters'),
-    body('confirmPassword').custom((confirmPassword, {req}) => {
+    body('confirmPassword').custom((confirmPassword, { req }) => {
         if (confirmPassword != req.body.password) {
-            throw new Error('Passwords not match!');
+            throw new Error('Passwords do not match!');
         }
         return true;
     }),
-
     (req, res, next) => {
         let message = getErrorMessage(req);
         if (message) {
             return res.render('reset-password', { message });
         }
         next();
-        controller.resetPassword 
-    );
+    },
+    controller.resetPassword
+);
+
 
 module.exports = router;
